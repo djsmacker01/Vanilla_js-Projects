dafedf530a7f63caf5c48932b59f18638d73b1d3
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-12-28 18:07:21 +0000

    modified

--- function.js
+++ function.js
@@ -1,7 +1,22 @@
 // Function expression
-let sayHi = function () {
-    alert('Hello world')
-};
+// let sayHi = function () {
+//     alert('Hello world')
+// };
 
-sayHi();
+// sayHi();
 // alert(sayHi);
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+function showOk() {
+    alert("You Agreed")
+}
+
+function showCancel() {
+    alert("You canceled the execution")
+}
+
+ask("Do you agree? ", showOk, showCancel)
