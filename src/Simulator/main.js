52be2ef817e2ceb9607b750129764d8af10857c1
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-03 19:19:11 +0000

    init commit

--- src/Simulator/main.js
+++ src/Simulator/main.js
@@ -4,6 +4,8 @@ function run() {
     const fName = document.querySelector('#firstName').value
     const lName = document.querySelector('#lastName').value
     const mName = document.querySelector('#middleName').value
+    const age = document.querySelector('#age').value
     
+    document.querySelector('#container').innerHTML = `My Names are ${fName} ${mName} ${mName}, i'am ${age} years old`
 
 }
