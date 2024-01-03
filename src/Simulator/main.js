d9f70d830ff74633febb18837f243610b5d209cf
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-03 20:04:55 +0000

    update

--- src/Simulator/main.js
+++ src/Simulator/main.js
@@ -1,11 +1,26 @@
-document.querySelector('#sim').addEventListener('click', run)
+// document.querySelector('#sim').addEventListener('click', run)
 
-function run() { 
-    const fName = document.querySelector('#firstName').value
-    const lName = document.querySelector('#lastName').value
-    const mName = document.querySelector('#middleName').value
-    const age = document.querySelector('#age').value
+// function run() {
+//     const fName = document.querySelector('#firstName').value
+//     const lName = document.querySelector('#lastName').value
+//     const mName = document.querySelector('#middleName').value
+//     const age = document.querySelector('#age').value
     
-    document.querySelector('#container').innerHTML = `My Names are ${fName} ${mName} ${mName}, i'am ${age} years old`
+//     document.querySelector('#container').innerHTML = `My Names are ${fName} ${mName} ${mName}, i'am ${age} years old`
 
+// }
+const test = document.querySelector('#firstName')
+// console.log(test)
+document.querySelector('#sim').addEventListener('click', run);
+
+function run() {
+    const fName = document.querySelector('#firstName').value;
+    const lName = document.querySelector('#lastName').value;
+    const mName = document.querySelector('#middleName').value;
+    const age = document.querySelector('#age').value;
+
+    document.querySelector('#container').innerHTML = ` ${fName} ${mName} ${lName}  ${age} `;
 }
+
+
+
