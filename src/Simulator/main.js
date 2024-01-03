6dd4f01edb74e60ee1cf7fc53f77e42df37f3dad
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-03 19:14:23 +0000

    new commit

--- src/Simulator/main.js
+++ src/Simulator/main.js
@@ -1,6 +1,9 @@
 document.querySelectorAll('#sim').addEventListener('click', run)
 
 function run() { 
-
+    const fName = document.querySelector('#firstName').value
+    const lName = document.querySelector('#lastName').value
+    const mName = document.querySelector('#middleName').value
     
+
 }
