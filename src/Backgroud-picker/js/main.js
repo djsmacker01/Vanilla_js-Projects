a6465ed5f08099513d56f41dec1a1e1955f0d8d6
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-04 13:03:59 +0000

    new commit

--- src/Backgroud-picker/js/main.js
+++ src/Backgroud-picker/js/main.js
@@ -5,9 +5,18 @@ const colorGreen = () => {
     document.querySelector('body').style.color = 'white'
 }
 
+const colorBlue = () => { 
+    document.querySelector('body').style.backgroundColor = 'blue'
+    document.querySelector('body').style.color = 'white'
+}
+
+const colorPurple = () => { 
+    document.querySelector('body').style.backgroundColor = 'purple'
+    document.querySelector('body').style.color = 'white'
+}
 
 document.getElementById('green').onclick = colorGreen;
 document.getElementById('purple').onclick = colorPurple;
 document.getElementById('blue').onclick = colorBlue;
 
-console.log(test)
+console.log(colorBlue);
