c35e5823dba52e4b33780c05e2c05a79a9f82ca2
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-04 13:00:56 +0000

    update main.js

--- src/Backgroud-picker/js/main.js
+++ src/Backgroud-picker/js/main.js
@@ -1,4 +1,12 @@
-const test = document.getElementById('green').onclick = colorGreen;
+
+
+const colorGreen = () => {
+    document.querySelector('body').style.backgroundColor = 'green'
+    document.querySelector('body').style.color = 'white'
+}
+
+
+document.getElementById('green').onclick = colorGreen;
 document.getElementById('purple').onclick = colorPurple;
 document.getElementById('blue').onclick = colorBlue;
 
