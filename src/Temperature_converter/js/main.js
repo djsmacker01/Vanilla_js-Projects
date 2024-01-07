415ba33711449cbb4448f0cf959fa63feb5ee984
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-07 16:45:17 +0000

    New commit

--- src/Temperature_converter/js/main.js
+++ src/Temperature_converter/js/main.js
@@ -1,6 +1,7 @@
 const convert = () => {
     let temp = document.querySelector('#temp').value;
-    temp = temp * 9/5 + 32
+    temp = temp * 9 / 5 + 32
+    document.querySelector('#container').innerHTML = temp;
 }
 
 document.querySelector('#sim').addEventListener('click', convert);
