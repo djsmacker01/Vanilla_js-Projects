305410f7bd141fa345a62d30547601fc9aeb51c2
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-07 21:12:42 +0000

    commit message

--- src/bachelor-code/3/js/main.js
+++ src/bachelor-code/3/js/main.js
@@ -1,11 +1,31 @@
-const contestants = document.querySelectorAll('.contestant')
+// const contestants = document.querySelectorAll('.contestant')
 
-Array.from(contestants).forEach(element => element.addEventListener('click', checkForRose))
+// Array.from(contestants).forEach(element => element.addEventListener('click', checkForRose))
 
-function checkForRose(click){
-	if(click.target.classList.contains('rose')){
-		document.querySelector('#nikki').classList.toggle('hidden')
-	}else{
-		alert("Wrong!");
-	}
+// function checkForRose(click){
+// 	if(click.target.classList.contains('rose')){
+// 		document.querySelector('#nikki').classList.toggle('hidden')
+// 	}else{
+// 		alert("Wrong!");
+// 	}
+// }
+
+const claire = document.querySelector('#claire');
+const nikki = document.querySelector('#nikki');
+
+ document.querySelector('#contPrev').addEventListener('click', contPrev)
+// console.log(test)
+	// .
+
+document.querySelector('#contNext').addEventListener('click', contNext)
+// console.log(test1)
+function contNext() {
+	claire.classList.add('hidden')
+	alert('Ops! Wrong choice')
 }
+
+function contPrev() {
+	claire.classList.add('hidden')
+	nikki.classList.toggle('hidden')
+}
+
