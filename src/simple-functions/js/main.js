001b0d605cbef996a2367e1a8615b6c6a962af18
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-22 10:46:01 +0000

    New commitment

--- src/simple-functions/js/main.js
+++ src/simple-functions/js/main.js
@@ -1,12 +1,41 @@
 //---Easy
 //create a function that subtracts two numbers and alerts the difference
 
+function subtract(a, b) { 
+    alert(a - b);
+
+}
+// subtract(1, 2);
+
+
 //create a function that divides three numbers and console logs the quotient
 
-//create a function that multiplys three numbers and returns the product
+function divide(a, b, c) { 
+ console.log(a / b / c)
+}
 
+// divide (30,15,3)
+//create a function that multiplys three numbers and returns the product
+function multiply(a, b, c) { 
+    return a * b * c; 
+
+}
+let result = multiply(30,15,3);
+console.log(multiply(8,2,3))
+
+function turnMoney() {
+let result = multiply(30,15,3);
+    alert('£' + result);
+}
+// turnMoney();
 //---Medium
 //create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the third number
+function Add_N_divide(a, b, c) { 
 
+    return (a + b) % c;
+
+} 
+
+console.log(Add_N_divide(10, 23, 13))
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
--- src/sir-savage-the-21st/js/main.js
+++ src/sir-savage-the-21st/js/main.js
@@ -1,2 +1,12 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 //Bonus can you make it print '21' 21 times to the dom?
+// for (let i = 1; i <= 21; i++) {
+    
+// }
+
+function printLoop() {
+    for (let i = 1; i <= 21; i++) {
+    console.log(i);
+}
+}
+printLoop();
