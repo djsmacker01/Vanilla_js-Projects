ca700de7f63f430be546b81500a9cc4b9811b0fe
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-29 10:41:58 +0000

    new commit

--- Array/main.js
+++ Array/main.js
@@ -1,4 +1,10 @@
 //  Array is a data type use to store set of  elements.
 const movies_list = ["24 Hour", "Prison Break", "Legend of the seeker", "Power"]
 
-console.log(movies_list)
+movies_list.forEach(movie => {
+console.log(movie)
+    
+})
+
+// Getting the number of element stored in arrays
+console.log('Numbers of Element present in array:',movies_list.length)
