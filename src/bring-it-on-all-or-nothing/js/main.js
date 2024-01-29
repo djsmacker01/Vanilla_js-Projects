18901f50f784205757044b2d9b6f44d6fc1ff98d
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-29 13:24:44 +0000

    new commit

--- src/bring-it-on-all-or-nothing/js/main.js
+++ src/bring-it-on-all-or-nothing/js/main.js
@@ -1,30 +1,30 @@
 // *Variables*
 // Declare a variable, assign it a boolean, and alert the value
 
-let booleanValue = false;
+// let booleanValue = false;
 // alert(booleanValue)
 // Declare a variable, reassign it to your favorite color, and console log the value
 
-let favoriteColor = "red"
-favoriteColor = "blue"
-console.log(`Favorite ${favoriteColor}`)
+// let favoriteColor = "red"
+// favoriteColor = "blue"
+// console.log(`Favorite ${favoriteColor}`)
 
 // *Functions*
 // Create a function that takes in 4 numbers and returns the sum of the first 3 numbers divided by the fourth. Return the result. Call the function.
-function sumThreeNumbers(a,b,c,d) {
-    return (a + b + c + d) / 4;
-}
-sumThreeNumbers(30,40,33,12)
+// function sumThreeNumbers(a,b,c,d) {
+//     return (a + b + c + d) / 4;
+// }
+// sumThreeNumbers(30,40,33,12)
 
 // Create a function that takes in 2 numbers. Console log the first number to the power of the second. Call the function.
 
-function twoNumbers(a, b) { 
-    console.log(Math.pow(a,b));
-}
-twoNumbers(30,40)
+// function twoNumbers(a, b) { 
+//     console.log(Math.pow(a,b));
+// }
+// twoNumbers(30,40)
 // *Conditionals*
 // Create a function that takes in a boolean and a string. If the boolean is true, alert the string. If the boolean is false, console log the string
-function booleanToString(bool, string) {
+// function booleanToString(bool, string) {
     // if (bool) {
     //     alert(String)
     // }
@@ -33,7 +33,7 @@ function booleanToString(bool, string) {
     // }
 
 //   bool ? alert(string) : console.log(string)
-}
+// }
 
 //*Loops*
 //Create a function that takes in a number. 
@@ -42,6 +42,21 @@ function booleanToString(bool, string) {
 // instead of that number, if the number is divisible by 5 log "buzz" 
 // instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" 
 // instead of that number
-function takesNumber(number) { 
-      
+function fizzBuzz(number) { 
+    for (let i = 1; i < number; i++) {
+        //  console.log(i)
+        if (i % 3 === 0) {
+            console.log('fizz')
+        }
+        else if (i % 5 === 0) {
+            console.log('buzz')
+        }
+        else if (i % 3 === 0 &&  i % 5 === 0) { 
+           console.log('FizzBuzz')
+        }
+        else {
+            console.log(i)
+        }
+     } 
 }
+fizzBuzz(16)
