97b55c4a06fd95b944a3472bc8e8a790bbd8f1f8
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-29 21:10:21 +0000

    new commit

--- src/arrays-fight-to-the-finish/js/main.js
+++ src/arrays-fight-to-the-finish/js/main.js
@@ -7,8 +7,17 @@ movieTitles.forEach(movieTitle => {
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
 let numArray = [2, 4, 6, 8, 10, 12, 14]
 
+// for (let i = 0; i < numArray.length; i++){ 
+
+// }
+numArray.forEach((item, index) => {
+    numArray[index] = item + 3
+})
+
+//Find the average of all the numbers from question two
+
+let sum = 0
+
 for (let i = 0; i < numArray.length; i++){
-
+    sum
 }
-
-//Find the average of all the numbers from question three
