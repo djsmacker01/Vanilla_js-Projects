f3d2a3e1581e8b38b1d4e2d4fb96256c71ac8caf
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-29 13:10:47 +0000

    new commit

--- src/bring-it-on-all-or-nothing/js/main.js
+++ src/bring-it-on-all-or-nothing/js/main.js
@@ -36,4 +36,12 @@ function booleanToString(bool, string) {
 }
 
 //*Loops*
-//Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
+//Create a function that takes in a number. 
+// Console log all values from 1 to that number,
+// but if the number is divisible by 3 log "fizz" 
+// instead of that number, if the number is divisible by 5 log "buzz" 
+// instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" 
+// instead of that number
+function takesNumber(number) { 
+      
+}
