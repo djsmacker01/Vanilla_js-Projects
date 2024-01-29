08ae106f698c4fe57566ed37023f3384b488a7a7
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-29 12:48:28 +0000

    new commit

--- src/bring-it-on-all-or-nothing/js/main.js
+++ src/bring-it-on-all-or-nothing/js/main.js
@@ -2,17 +2,26 @@
 // Declare a variable, assign it a boolean, and alert the value
 
 let booleanValue = false;
-alert(booleanValue)
+// alert(booleanValue)
 // Declare a variable, reassign it to your favorite color, and console log the value
 
+let favoriteColor = "red"
+favoriteColor = "blue"
+console.log(`Favorite ${favoriteColor}`)
 
 // *Functions*
 // Create a function that takes in 4 numbers and returns the sum of the first 3 numbers divided by the fourth. Return the result. Call the function.
-
+function sumThreeNumbers(a,b,c,d) {
+    return (a + b + c + d) / 4;
+}
+sumThreeNumbers(30,40,33,12)
 
 // Create a function that takes in 2 numbers. Console log the first number to the power of the second. Call the function.
 
-
+function twoNumbers(a, b) { 
+    console.log(Math.pow(a,b));
+}
+twoNumbers(30,40)
 // *Conditionals*
 // Create a function that takes in a boolean and a string. If the boolean is true, alert the string. If the boolean is false, console log the string
 
