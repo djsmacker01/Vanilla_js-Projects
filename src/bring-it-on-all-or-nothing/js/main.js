5c6b8a122ce23f01f58474601659ccd28148cbe6
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-29 12:53:43 +0000

    new commit

--- src/bring-it-on-all-or-nothing/js/main.js
+++ src/bring-it-on-all-or-nothing/js/main.js
@@ -24,7 +24,16 @@ function twoNumbers(a, b) {
 twoNumbers(30,40)
 // *Conditionals*
 // Create a function that takes in a boolean and a string. If the boolean is true, alert the string. If the boolean is false, console log the string
+function booleanToString(bool, string) {
+    // if (bool) {
+    //     alert(String)
+    // }
+    // else {
+    //     console.log(string)
+    // }
 
+//   bool ? alert(string) : console.log(string)
+}
 
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number, but if the number is divisible by 3 log "fizz" instead of that number, if the number is divisible by 5 log "buzz" instead of the number, and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
