49ea579f0c595e9df292f6815f98598d84ee8204
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-01-29 11:04:17 +0000

    new commit

--- Array/main.js
+++ Array/main.js
@@ -1,9 +1,17 @@
-// Updating Array contents
+
 
 
 //  Array is a data type use to store set of  elements.
 // Iterating over an array
 const movies_list = ["24 Hour", "Prison Break", "Legend of the seeker", "Power"]
+// Updating Array contents
+movies_list.push('Rush Hour')
+// To add new movies at the beginning of the list
+movies_list.unshift('Marlin')
+
+// Remove last elements from the list
+// movies_list.pop()
+movies_list.splice(2)
 
 movies_list.forEach(movie => {
 console.log(movie)
