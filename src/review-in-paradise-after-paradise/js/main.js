bc36892e3a649fd4ad2e89f5b06c25caca649ef1
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-04 20:56:46 +0000

    new commit

--- src/review-in-paradise-after-paradise/js/main.js
+++ src/review-in-paradise-after-paradise/js/main.js
@@ -2,3 +2,5 @@
 //If the first number, is less than the last number, alert 
 //"Hi". If the first number is greater than the last number, alert "Bye". 
 //If they are equal, alert "We close in an hour".
+let nums = [1, 2, 3, 4, 5, 6, 7, 8]
+ 
