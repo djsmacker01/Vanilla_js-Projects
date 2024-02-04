be4a60b36f5b014e74daf11e8e4736cec12eb2eb
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-04 22:12:51 +0000

    new commit

--- src/review-summer-games/js/main.js
+++ src/review-summer-games/js/main.js
@@ -1,1 +1,6 @@
-//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+//Create a function that takes in an array of numbers.
+// Multiply each number together and alert the product. 
+
+function arrTwoNum(arr) { 
+    
+}
