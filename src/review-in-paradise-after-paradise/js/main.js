d280877af4e561e58959ef8c147eb6b5b0bf6991
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-04 20:58:41 +0000

    new commit

--- src/review-in-paradise-after-paradise/js/main.js
+++ src/review-in-paradise-after-paradise/js/main.js
@@ -3,4 +3,8 @@
 //"Hi". If the first number is greater than the last number, alert "Bye". 
 //If they are equal, alert "We close in an hour".
 // let nums = [1, 2, 3, 4, 5, 6, 7, 8]
+
+function firstAndLast(arr) {
+    
+}
  
