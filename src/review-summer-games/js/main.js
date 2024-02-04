60ace1d89ef0eb11a4f5281ca6ea196ceb2b4e03
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-04 22:18:20 +0000

    new commit

--- src/review-summer-games/js/main.js
+++ src/review-summer-games/js/main.js
@@ -1,6 +1,12 @@
 //Create a function that takes in an array of numbers.
 // Multiply each number together and alert the product. 
 
-function arrTwoNum(arr) { 
-    
+function arrTwoNum(arr) {
+    let product = 1
+    arr.forEach(element => {
+        product *= element
+        alert(product)
+    });
 }
+arrTwoNum([10,20,10])
+
