0914f8ddb6b31338360e48a1c2981421ca74b9d1
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-05 18:05:16 +0000

    new commit

--- src/review-summer-games/js/main.js
+++ src/review-summer-games/js/main.js
@@ -3,10 +3,13 @@
 
 function arrTwoNum(arr) {
     let product = 1
-    arr.forEach(element => {
-        product *= element
-        alert(product)
-    });
+    // arr.forEach(element => {
+    //     product *= element
+    //     alert(product)
+    // });
+    for (let i = 0; i < arr.length; i++) { 
+        product *= arr[i];
+    }
 }
 arrTwoNum([10,20,10])
 // arrTwoNum([30,80,10])
