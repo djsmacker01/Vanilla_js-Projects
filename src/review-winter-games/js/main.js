0116f57a1db8553037371841d1999e6fda83d28e
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-06 17:13:52 +0000

    new commit message

--- src/review-winter-games/js/main.js
+++ src/review-winter-games/js/main.js
@@ -1,1 +1,14 @@
-//Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+//Create a function that takes in an array of numbers. 
+//Return a new array containing every even number from the original 
+//array(do not use map or filter)
+
+
+function evenNumber(number) {
+    let eNum = []
+    for (let i = 0; i < number.length; i++) {
+        if (number[i] % 2 == 0) {
+            eNum.push(number[i])
+        }
+    }
+    return eNum
+}
