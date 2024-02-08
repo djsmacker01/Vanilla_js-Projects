102a4fcba12c06e74c4cc966adcd802ed49f3f0a
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-08 08:54:04 +0000

    new commit message

---
+++ template/js/main.js
@@ -0,0 +1,7 @@
+// Write the function camelize(str) that changes dash - separated words like
+// “my - short - string” into camel - cased “myShortString”.
+Examples:
+
+camelize("background-color") == 'backgroundColor';
+camelize("list-style-image") == 'listStyleImage';
+camelize("-webkit-transition") == 'WebkitTransition';
