c92e1b6f0f083b74d35c909f039dc1a93e1957ea
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-08 21:43:17 +0000

    new commit

--- src/objects-zeo/js/main.js
+++ src/objects-zeo/js/main.js
@@ -1,1 +1,12 @@
 //Create a stopwatch object that has four properties and three methods
+const stopWatch = {}
+
+stopWatch.brand = 'Nike'
+stopWatch.color = 'black'
+stopWatch.shape = 'square'
+stopWatch.size = 'medium'
+
+
+stopWatch.start = function () {
+    
+}
