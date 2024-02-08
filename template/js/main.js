e1682a2c4edab689400072adecb4bfce7e295352
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-08 09:16:30 +0000

    new commit

--- template/js/main.js
+++ template/js/main.js
@@ -10,6 +10,9 @@ camelize("-webkit-transition") == 'WebkitTransition';
 const camelize = (str) => {
     return str
         .split('-') //split 'my-long-word' into array ['my','long', 'word']
-        .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1))
-        .join(' ')
-}   
+        .map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1))
+        .join('')
+}  
+
+let words = "Java-Script-compliers"
+console.log(camelize(words))
