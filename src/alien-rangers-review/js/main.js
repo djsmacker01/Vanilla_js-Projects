de272297fdd6bb95f50409f3254257ed4484c46e
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-08 12:36:28 +0000

    commit

--- src/alien-rangers-review/js/main.js
+++ src/alien-rangers-review/js/main.js
@@ -1,6 +1,9 @@
 //Arrays
 
 //Create and array of tv shows. Loop through and print each show to the console
+let tvShows = ['Vampire Dairy', 'Black list', 'Prison break', '24 Hour'];
+//  tvShows.forEach(show=> console.log(show))
+ tvShows.map((item) => console.log(item))
 
 //Create and array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
