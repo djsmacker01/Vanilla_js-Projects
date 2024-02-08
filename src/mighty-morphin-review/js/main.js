203b474ff551a4ce7712613ca24978f941de7c56
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-08 11:53:22 +0000

    commit

--- src/mighty-morphin-review/js/main.js
+++ src/mighty-morphin-review/js/main.js
@@ -6,42 +6,44 @@ let caps = val.toUpperCase()
 console.log(caps)
 //Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
 
-let stringChar = 'javascript'
-let newStr = stringChar.slice(-3)
-alert(newStr)
+// let stringChar = 'javascript'
+// let newStr = stringChar.slice(-3)
+// alert(newStr)
 
 // *Functions*
 // Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
-function subFiveNumbers(n1, n2, n3, n4, n5) { 
-    let difference = 100 - n1 - n2 - n3 - n4 - n5;
+// function subFiveNumbers(n1, n2, n3, n4, n5) { 
+//     let difference = 100 - n1 - n2 - n3 - n4 - n5;
 
-    alert(Math.abs(difference))
-}
-subFiveNumbers(40,76,80,98,22)
+//     alert(Math.abs(difference))
+// }
+// subFiveNumbers(40,76,80,98,22)
 
 // Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
-function lowestAndHighest(n1, n2, n3) {
-    let min = Math.min(n1, n2, n3);
-    let max = Math.max(n1, n2, n3);
+// function lowestAndHighest(n1, n2, n3) {
+//     let min = Math.min(n1, n2, n3);
+//     let max = Math.max(n1, n2, n3);
 
-    console.log(`The lowest number is:  ${min} and the highest is:  ${max}`);
-}
-lowestAndHighest(100,160,893)
+//     console.log(`The lowest number is:  ${min} and the highest is:  ${max}`);
+// }
+// lowestAndHighest(100,160,893)
 
 // *Conditionals*
 //Create a function that returns heads or tails randomly and as fairly as possible.
 //  Call the function.
-function headsOrTails() {
-    let result = Math.random();
-    if (result < 0.5) {
-        return 'heads'
+// function headsOrTails() {
+//     let result = Math.random();
+//     if (result < 0.5) {
+//         return 'heads'
         
-    }
-    else {
-        return 'tails'
-    }
+//     }
+//     else {
+//         return 'tails'
+//     }
     
-}
+// }
+
+const headsOrTails = ()=> Math.random < .5 ? 'heads' : 'tails';
 // console.log(headsOrTails())
 
 
