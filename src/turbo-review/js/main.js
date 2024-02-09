a3f5e2ce28ba254fad528e87391bb6101e77f6de
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-09 14:26:46 +0000

    new commit message

--- src/turbo-review/js/main.js
+++ src/turbo-review/js/main.js
@@ -2,14 +2,21 @@
 // Declare a variable and assign it to your fav drink as a string. 
 //Make sure there is no whitespace on either side of the string, and print the value to the console
 
-let favoriteDrink = '                    Soya Milk '
+let favoriteDrink = ' Soya Milk '
 favoriteDrink = favoriteDrink.trim()
 console.log(favoriteDrink)
 
 //Declare a variable, assign it a string of multiple words, and check to see if one of the words is "apple".
-let val
+let str = 'mate egg printer apple jez'
+if (str.charAt('apple') !== -1) {
+    console.log('Yes')
+} else {
+    consol.log('No')
+}
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
+
+
 
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
