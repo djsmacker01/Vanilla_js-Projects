550415022a160d2e437eee762f4284218e06cdcc
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-09 14:02:18 +0000

    new commit

--- src/turbo-review/js/main.js
+++ src/turbo-review/js/main.js
@@ -1,8 +1,13 @@
 // *Variables*
-// Declare a variable and assign it to your fav drink as a string. Make sure there is no whitespace on either side of the string, and print the value to the console
+// Declare a variable and assign it to your fav drink as a string. 
+//Make sure there is no whitespace on either side of the string, and print the value to the console
+
+let favoriteDrink = '                    Soya Milk '
+favoriteDrink = favoriteDrink.trim()
+console.log(favoriteDrink)
 
 //Declare a variable, assign it a string of multiple words, and check to see if one of the words is "apple".
-
+let val
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
 
