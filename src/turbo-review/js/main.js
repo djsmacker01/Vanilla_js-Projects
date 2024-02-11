ca5f74223b46627ad8594e49276a7ea1351d23ba
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-11 15:08:29 +0000

    commit

--- src/turbo-review/js/main.js
+++ src/turbo-review/js/main.js
@@ -50,7 +50,7 @@ function choice(playerChoice) {
     }
 
 }
-choice()
+choice('paper')
 
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
