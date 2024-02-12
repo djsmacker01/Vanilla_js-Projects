bdd418f267de7aaeb4c884e796b6e8469ae5db43
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-12 20:13:36 +0000

    commit

--- src/objects-street-fighter/js/main.js
+++ src/objects-street-fighter/js/main.js
@@ -1,1 +1,2 @@
-//Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+//Create a street fighter constructor that makes fighting game 
+//characters with 4 properties and 3 methods
