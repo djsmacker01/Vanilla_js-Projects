45fe6d44b73bcafc8b849214fe1d2e95cd8bf795
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-12 20:19:19 +0000

    commit

--- src/objects-street-fighter/js/main.js
+++ src/objects-street-fighter/js/main.js
@@ -1,2 +1,9 @@
 //Create a street fighter constructor that makes fighting game 
 //characters with 4 properties and 3 methods
+
+function streetFighter(punch,kick, taunt, specialMove) {
+    this.punch = punch
+    this.kick = kick
+    this.specialMove = specialMove
+    this. taunt = taunt
+}
