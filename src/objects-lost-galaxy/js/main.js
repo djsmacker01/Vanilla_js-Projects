0f0c7fe95352a7296cbd9dd490f0c1c33a609df9
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-12 18:17:13 +0000

    commit

--- src/objects-lost-galaxy/js/main.js
+++ src/objects-lost-galaxy/js/main.js
@@ -1,10 +1,19 @@
 //Create a mouse object that has four properties and three methods
 
-const Mouse = {
+const mouse = {
+    
+}
     mouse.brand = 'Dell'
     mouse.color = 'black'
     mouse.model =  'Mx Ergo'
-}
 mouse.leftClick = function () {
      console.log('left click')
+ }
+
+ mouse.rightClick = function () {
+     console.log('right click')
+ }
+
+ mouse.scroll = function () {
+     console.log('scroll up and down')
  }
