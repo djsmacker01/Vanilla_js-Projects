7fed65fe657805d7b7a2cbfc229d4b733d6a91b1
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-13 13:37:55 +0000

    commit

--- src/JS-OOP/StopWatch Object/index.js
+++ src/JS-OOP/StopWatch Object/index.js
@@ -2,17 +2,26 @@ function StopWatch() {
     let startTime, endTime, running, duration = 0
     
     this.start = function () {
-        
+        if (running) 
+            throw new Error('StopWatch is already running')
+        running = true
+        startTime = new Date()
     }
 
     this.stop = function () {
+        if (!running)
+            throw new Error('StopWatch has not started.')
+        running = false
+        endTime = new Date()
+        const elapse = (endTime.getTime() - startTime.getTime()) / 1000
+        duration += elapse
         
     }
     this.pause = function () { 
 
     }
     this.reset = function () { 
-        
+
     }
 
 }
