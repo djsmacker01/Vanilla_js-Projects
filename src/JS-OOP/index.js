53a0927f6234048930afa0ab209c006ed56a6aa9
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-13 10:40:12 +0000

    commit

--- src/JS-OOP/index.js
+++ src/JS-OOP/index.js
@@ -1,13 +1,15 @@
 // Object literal
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  draw: function () {
-    console.log("draw");
-  },
-};
+// const circle = {
+//   radius: 1,
+//   location: {
+//     x: 1,
+//     y: 1,
+//   },
+//   draw: function () {
+//     console.log("draw");
+//   },
+// };
 
-circle.draw()
+// circle.draw()
+
+// Factory or contructor function 
