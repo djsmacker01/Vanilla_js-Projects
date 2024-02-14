ec0d23deef87e137963de7426de800107001bca0
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-14 13:57:28 +0000

    commit

--- src/objects-dog-photos/js/main.js
+++ src/objects-dog-photos/js/main.js
@@ -2,7 +2,8 @@
 fetch("https://dog.ceo/api/breeds/image/random")
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-       console.log(data);  
+        console.log(data.message);  
+        document.querySelector('img').src = data.message
     })
     .catch(err => {
       console.log(`error: ${err}`)  
