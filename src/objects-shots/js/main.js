457e076fc67e124f57bc91e152d1115f4e051bc0
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-19 11:28:54 +0000

    commit

--- src/objects-shots/js/main.js
+++ src/objects-shots/js/main.js
@@ -5,6 +5,7 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
     .then(data => {
         console.log(data.drinks[0]);
         document.querySelector('h2').innerHTML = data.drinks[0].strDrink
+        document.querySelector('h3').innerHTML = data.drinks[0].strInstructions
         document.querySelector('img').src = data.drinks[0].strDrinkThumb
     })
     .catch(err => {
