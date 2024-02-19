745eb4836952261152dcdcaaa6f45b4c98dcd5cb
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-19 11:35:22 +0000

    commit

--- src/objects-shots/js/main.js
+++ src/objects-shots/js/main.js
@@ -1,6 +1,11 @@
 //The user will enter a cocktail. Get a cocktail name, 
 //photo, and instructions and place them in the DOM
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+
+document.querySelector('button').addEventListener('click', getDrink)
+
+function getDrink() {
+    let drink = document.querySelector('input').value
+fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     .then(res => res.json())
     .then(data => {
         console.log(data.drinks[0]);
@@ -11,3 +16,5 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
     .catch(err => {
         console.log(`Error: ${err}`);
     })
+
+}
