b8f592235a1274ae73b3ad3319a4cf8bf1b2533e
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-02-21 12:04:02 +0000

    commit

--- src/objects-shots/js/index.js
+++ src/objects-shots/js/index.js
@@ -1,3 +1,10 @@
 // Add event listeners
 
 document.querySelectors('button').addEventListener('click', getDrinks)
+
+// using the enter key to fire an event from the input element
+document.querySelector('input').addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') { 
+        document.querySelector('button').click();
+    }
+})
