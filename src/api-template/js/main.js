211599494cde2808371aa353e4280bad6ba9bc62
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-03-03 16:49:32 +0000

    commit

--- src/api-template/js/main.js
+++ src/api-template/js/main.js
@@ -3,7 +3,9 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value.toLowerCase()
-  const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
+  // const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
+  const url = `https://api.nasa.gov/planetary/apod?api_key=gvuajxlFDJeFc9PdVMIgmmCeXYXEhvUpo6huKhil`
+
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
