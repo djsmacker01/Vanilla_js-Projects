fc16d9cbdd559ebf4983cb405bcba54d48853549
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-03-03 14:14:29 +0000

    commit

--- src/objects-netflix-tv-shows/js/main.js
+++ src/objects-netflix-tv-shows/js/main.js
@@ -1,4 +1,5 @@
-//Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
+//Create a Netflix TV Show class with a constructor that 
+//makes Netflix TV Shows with 4 properties and 3 methods
 
 class MakeNetflixTvShows{
     constructor(title, genre,rating,numOfEpisodes) {
@@ -8,5 +9,5 @@ class MakeNetflixTvShows{
         this.numOfEpisodes = numOfEpisodes;
     }
 
-    
+
 }
