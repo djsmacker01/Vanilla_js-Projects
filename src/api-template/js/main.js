30b024b22a4ebf58de8a23d2706d130226d8d19e
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-03-03 16:11:52 +0000

    commit

--- src/api-template/js/main.js
+++ src/api-template/js/main.js
@@ -2,8 +2,8 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const choice = document.querySelector('input').value.toLowerCase()
+  const url = `https://pokeapi.co/api/v2/pokemon/'+${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
