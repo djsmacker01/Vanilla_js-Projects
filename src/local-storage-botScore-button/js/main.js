7c35d2203ceb0fde0491cd2917384784fea7ec95
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-03-04 13:06:18 +0000

    commit

--- src/local-storage-botScore-button/js/main.js
+++ src/local-storage-botScore-button/js/main.js
@@ -1,11 +1,13 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
 localStorage.setItem('botScore',0)
 
-//Example fetch using pokemonapi.co
+
 document.querySelector('button').addEventListener('click', addAnotherOne)
 
 function addAnotherOne(){
-
+    let botScoreVal = localStorage.getItem('botScore')
+    botScoreVal += 1
+    localStorage.setItem('botScore', botScoreVal)
 }
 
 
