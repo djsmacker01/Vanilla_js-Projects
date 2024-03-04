64edba207ffe76e7291a6639d7c193d43e1e3dad
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-03-04 16:59:14 +0000

    commit

--- src/card-game/js/main.js
+++ src/card-game/js/main.js
@@ -2,20 +2,30 @@ let deckId = ' '
  fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
       .then(res => res.json()) // parse response as JSON
     .then(data => {
-        console.log(data)
+      console.log(data)
+      deckId = data.deck_id
        
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+
 document.querySelector('button').addEventListener('click', drawTwo)
 
 function drawTwo(){
-  const choice = document.querySelector('input').value
-  console.log(choice)
+  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+      .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+      document.querySelector('#player1').src = data.cards[0].image
+      document.querySelector('#player2').src = data.cards[1].image
+       
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
  
-  const url = `https://api.nasa.gov/planetary/apod?api_key=21DG9YugnkoCOwBp8nG4CbrHggWWaJVX1Ezmzhaw&date=${choice}`
-
+ 
 
  
 }
