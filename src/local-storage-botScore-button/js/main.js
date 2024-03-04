cc14a70e37f5c76c6bbaaaae223891ff6fab3e72
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-03-04 13:16:26 +0000

    commit

--- src/local-storage-botScore-button/js/main.js
+++ src/local-storage-botScore-button/js/main.js
@@ -1,11 +1,14 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
+if (!localStorage.getItem('botScore')){
 localStorage.setItem('botScore',0)
+    
+}
 
 
 document.querySelector('button').addEventListener('click', addAnotherOne)
 
 function addAnotherOne(){
-    let botScoreVal = localStorage.getItem('botScore')
+    let botScoreVal = Number(localStorage.getItem('botScore'))
     botScoreVal += 1
     localStorage.setItem('botScore', botScoreVal)
 }
