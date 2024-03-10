df39a7426844aef570f49f82ee3dc401347857f7
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-03-10 17:09:13 +0000

    commit

--- src/dnd/js/main.js
+++ src/dnd/js/main.js
@@ -8,7 +8,9 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       console.log(data.subclasses[0]);
+          console.log(data.subclasses[0]);
+       console.log(data.subclasses[1]);
+          
       })
       .catch(err => {
           console.log(`error ${err}`)
