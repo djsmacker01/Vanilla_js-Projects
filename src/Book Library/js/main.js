23af98cca5b4aa93692d7c05727d502992b027c6
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-03-18 17:30:21 +0000

    commit

--- src/Book Library/js/main.js
+++ src/Book Library/js/main.js
@@ -5,17 +5,17 @@ function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
   // const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
-  const url = `https://openlibrary.org/isbn/9781444765427.json`
+  const url = `https://openlibrary.org/isbn/${choice}.json`
 
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
      .then(data => {
-        console.log(data)
+        console.log(data.title)
        
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
-// https://www.youtube.com/embed/5D9j-8Vhyto?rel=0&showinfo=0
+
