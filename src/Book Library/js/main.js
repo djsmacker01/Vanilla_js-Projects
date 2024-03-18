2323d92a5aa8b496752f198c5fa1e82d78645828
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-03-18 17:22:18 +0000

    commit

--- src/Book Library/js/main.js
+++ src/Book Library/js/main.js
@@ -5,7 +5,7 @@ function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
   // const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
-  const url = `https://api.nasa.gov/planetary/apod?api_key=21DG9YugnkoCOwBp8nG4CbrHggWWaJVX1Ezmzhaw&date=${choice}`
+  const url = `https://openlibrary.org/isbn/9781444765427.json`
 
 
   fetch(url)
