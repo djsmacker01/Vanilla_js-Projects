6d80d5f681710bea00bc78705448dc4601d433bd
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-03-26 22:26:17 +0000

    commit

--- src/cascade-badge/js/main.js
+++ src/cascade-badge/js/main.js
@@ -1,12 +1,13 @@
 //Arrays
 //Your pokemon party order which is a list of pokemon has been leaked to Misty.
 // Please create a function that reverses your list and prints it to the console. 
-
+// console.log("--------------------------------")
 function reversePartyOrder(partyList) {
-    console.log('PartyList :', partyList.reverse() )
+  
+    console.log(partyList.reverse() )
 }
 
-reversePartyOrder['Maths','English','Physics']
+reversePartyOrder(['Maths','English','Physics'])
 
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
 
