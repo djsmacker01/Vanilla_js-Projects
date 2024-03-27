2836e70a9dd8d43baa5b7244853b2edd4b775a3b
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-03-27 17:15:41 +0000

    commit

--- src/cascade-badge/js/main.js
+++ src/cascade-badge/js/main.js
@@ -9,7 +9,7 @@ function reversePartyOrder(partyList) {
 }
 
 reversePartyOrder(['Maths', 'English', 'Physics', 'Geography'])
-reversePartyOrder()
+reversePartyOrder(['Maths', 'English', 'Physics', 'Ge'])
 
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
 
