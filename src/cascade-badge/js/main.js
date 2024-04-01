f03915302767eca42857f4c705c0df0f4c7a6287
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-01 18:18:13 +0100

    commit

--- src/cascade-badge/js/main.js
+++ src/cascade-badge/js/main.js
@@ -9,7 +9,7 @@ function reversePartyOrder(partyList) {
 }
 
 reversePartyOrder(['Maths', 'English', 'Physics', 'Geography'])
-// reversePartyOrder(['Maths', 'English', 'Physics', 'Ge'])
+reversePartyOrder(['Maths', 'English', 'Physics', 'Ge'])
 
 //Given two integer arrays a, b, both of length >= 1,
 // create a program that returns true if the sum of the squares of each element 
