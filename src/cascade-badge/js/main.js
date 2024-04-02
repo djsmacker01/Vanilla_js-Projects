4777121c539489d79d0d19bcd830838a9cc37c1e
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-02 15:37:56 +0100

    commit

--- src/cascade-badge/js/main.js
+++ src/cascade-badge/js/main.js
@@ -21,7 +21,8 @@ function compareSquaresAndCubes(a, b) {
 
 console.log(compareSquaresAndCubes([4,5,3,4], [2,2,2]))
 
-//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+//Return a new array consisting of elements which are multiple 
+// of their own index in input array(length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
