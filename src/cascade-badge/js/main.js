de38f0ec88f166b86b5c4cef95f176d6bf381c52
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-02 15:57:35 +0100

    commit

--- src/cascade-badge/js/main.js
+++ src/cascade-badge/js/main.js
@@ -34,4 +34,6 @@ function isMultiple(arr) {
 //Given an array of integers as strings and numbers, return the sum of the array
 //values as if all were numbers.Return your answer as a number.
 
-
+function summArrayValue(arr) { 
+    return arr.reduce((acc,c)=> acc + c, 0)
+}
