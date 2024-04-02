6c227bacbaacdb06f01b126f424545156b09d374
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-02 15:51:31 +0100

    commit

--- src/cascade-badge/js/main.js
+++ src/cascade-badge/js/main.js
@@ -27,4 +27,6 @@ console.log(compareSquaresAndCubes([4,5,3,4], [2,2,2]))
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
 
+function
+
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
