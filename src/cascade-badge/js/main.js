06998cd9b6f8606287d3d921a58f43f0edddc372
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-02 14:55:52 +0100

    commit

--- src/cascade-badge/js/main.js
+++ src/cascade-badge/js/main.js
@@ -16,7 +16,7 @@ reversePartyOrder(['Maths', 'English', 'Physics', 'Ge'])
 // in a is strictly greater than the sum of the cubes of each element in b.
 
 function compareSquaresAndCubes(a, b) {
-    return a.reduce((acc,c)=> acc + c ** 2, 0) > b.reduce((acc,c)=> acc + c ** 3);
+    return a.reduce((acc,c)=> acc + c ** 2, 0) > b.reduce((acc,c)=> acc + c ** 3, 0);
 }
 
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
