596459d1a5af77b62929a41788b6f0d57c76764f
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-04 15:18:29 +0100

    commit

--- src/objects-espresso-machine/js/main.js
+++ src/objects-espresso-machine/js/main.js
@@ -2,8 +2,12 @@
 // that makes machines with 4 properties and 3 methods
 
 class EspressoMachine {
-    constructor(brand,color,cost,) {
+    constructor(brand,color,cost, type) {
         
+        this.brand = brand;
+        this.color = color;
+        this.cost = cost;
+        this.type = type;
     }
 
 }
