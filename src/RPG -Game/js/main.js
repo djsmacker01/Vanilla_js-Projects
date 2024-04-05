b83c1054e7634279e67c9543a662ddd0d5bc39a8
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-05 09:30:26 +0100

    add new js file

---
+++ src/RPG -Game/js/main.js
@@ -0,0 +1,25 @@
+//Create a function that takes in an array of numbers. 
+//Return a new array containing every even number from the original 
+//array(do not use map or filter)
+
+
+// function evenNumber(number) {
+//     let eNum = []
+//     for (let i = 0; i < number.length; i++) {
+//         if (number[i] % 2 == 0) {
+//             eNum.push(number[i])
+//         }
+//     }
+//     return eNum
+// }
+
+function evenNumber(num) {
+    let eNum = []
+
+    num.forEach(n => {
+        if (n % 2 === 0) {
+      eNum.push(n)
+      }
+    })
+    return eNum
+}
