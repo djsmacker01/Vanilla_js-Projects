b979142635342056272e271f766e0916ad211093
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-06 16:24:13 +0100

    new commit

--- src/JS-OOP/index.js
+++ src/JS-OOP/index.js
@@ -1,14 +1,14 @@
-// Object literal
-// const circle = {
-//   radius: 1,
-//   location: {
-//     x: 1,
-//     y: 1,
-//   },
-//   draw: function () {
-//     console.log("draw");
-//   },
-// };
+Object literal
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  draw: function () {
+    console.log("draw");
+  },
+};
 
 // circle.draw()
 
