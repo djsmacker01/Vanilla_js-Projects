4cf0552947a95a757d345af8af83344fb8d0cf52
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-13 17:51:29 +0100

    commit

--- src/async-practice/js/main.js
+++ src/async-practice/js/main.js
@@ -154,10 +154,17 @@ function houseThree(){
 // getPaid()
 
 //Code 08
-async function getACuteDogPhoto(){
-    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+async function getACuteDogPhoto() {
+    try {
+        const res = await fetch('https://dog.ceo/api/breeds/image/random')
     const data = await res.json()
+    
     console.log(data)
+    }
+    catch (error) {
+         console.error('Error fetching dog photo:', error);
+    }
+    
 }
 getACuteDogPhoto()
 
