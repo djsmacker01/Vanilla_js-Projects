5bb79e664754082e489d4ac88aeace9bd1bf4a0a
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-05-13 14:59:41 +0100

    commit

--- src/async-practice/js/main.js
+++ src/async-practice/js/main.js
@@ -57,48 +57,67 @@
 // houseOne()
 
 // Code 05
-const promise = new Promise((resolve, reject) => {
-    const error = false
-    if(!error){
-        resolve('Promise has been fullfilled')
-    }else{
-        reject('Error: Operation has failed')
-    }
-})
-console.log(promise)
-promise
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+// const promise = new Promise((resolve, reject) => {
+//     const error = false
+//     if(!error){
+//         resolve('Promise has been fullfilled')
+//     }else{
+//         reject('Error: Operation has failed')
+//     }
+// })
+// console.log(promise)
+// promise
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
 
-//Code 06
-// function houseOne(){
+// Code 06
+function houseOne(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Paper delivered to house 1')
+        }, 1000)
+    })
+}
+function houseTwo(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Paper delivered to house 2')
+        }, 5000)
+    })
+}
+function houseThree(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Paper delivered to house 3')
+        }, 2000)
+    })
+}
+
+// function houseFour() {
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
-//             resolve('Paper delivered to house 1')
-//         }, 1000)
+//             resolve('Goods deliver to house four')
+//         }, 4000)
 //     })
 // }
-// function houseTwo(){
+
+// function houseFive() {
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
-//             resolve('Paper delivered to house 2')
-//         }, 5000)
-//     })
-// }
-// function houseThree(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve('Paper delivered to house 3')
+//             resolve('Goods deliver to house five')
 //         }, 2000)
 //     })
 // }
-// houseOne()
+// houseFive
 //     .then(data => console.log(data))
-//     .then(houseTwo)
-//     .then(data => console.log(data))
-//     .then(houseThree)
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
+
+houseOne()
+    .then(data => console.log(data))
+    .then(houseTwo)
+    .then(data => console.log(data))
+    .then(houseThree)
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
 
 //Code 07
 // function houseOne(){
